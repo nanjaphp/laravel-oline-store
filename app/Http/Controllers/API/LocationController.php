8fdfc7d6d82848dab722d045b861bbc4dff8225b
ilya <ilay3109@gmail.com>
2022-05-23 17:21:41 +0300

    fix LocationController

--- app/Http/Controllers/API/LocationController.php
+++ app/Http/Controllers/API/LocationController.php
@@ -3,19 +3,18 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\Models\Province;
-use App\Models\Regency;
-use Illuminate\Http\Request;
+use App\Models\City;
+use App\Models\Region;
 
 class LocationController extends Controller
 {
     public function regions()
     {
-        return Province::all();
+        return Region::all();
     }
 
     public function cities($region_id)
     {
-        return Regency::where('region_id', $region_id)->get();
+        return City::where('region_id', $region_id)->get();
     }
 }
